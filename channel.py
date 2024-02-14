da7ec3e1df8a6063714e9d021f780bd6db86cda0
Lappus <juli.cornelsen@googlemail.com>
2024-02-14 20:52:36 +0100

    for real server deployment

--- channel.py
+++ channel.py
@@ -24,14 +24,11 @@ app = Flask(__name__)
 app.config.from_object(__name__ + '.ConfigClass')  # configuration
 app.app_context().push()  # create an app context before initializing db
 
-
-
-
-HUB_URL = 'http://localhost:5555'
-HUB_AUTHKEY = '1234567890'
+HUB_URL = 'https://temporary-server.de'
+HUB_AUTHKEY = 'Crr-K3d-2N'
 CHANNEL_AUTHKEY = '0987654321'
 CHANNEL_NAME = "MemeForge3000"
-CHANNEL_ENDPOINT = "http://localhost:5001" # don't forget to adjust in the bottom of the file
+CHANNEL_ENDPOINT = "http://vm150.rz.uni-osnabrueck.de/user168/channel.wsgi" # don't forget to adjust in the bottom of the file
 CHANNEL_FILE = 'messages.json'
 
 @app.cli.command('register')
